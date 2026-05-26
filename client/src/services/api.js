eb553ea118e529ad9d8353313c57356dbd0e0ffd
Sarvadnya95 <dattatidke95@gmail.com>
2026-05-26 12:28:12 +0530

    update backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://educore-backend-y6xs.onrender.com/api'
 })
 
 // Automatically add token to every request
